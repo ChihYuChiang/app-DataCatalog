81f7ada7d398363c9b91096e0c424374330b355c
ChihYuChiang <ckccyu@gmail.com>
2019-05-30 01:29:37 +0800

    Add: combine NNP

--- preprocessing/transform.py
+++ preprocessing/transform.py
@@ -11,12 +11,12 @@ from gensim.corpora import Dictionary
 #--Loading data back
 #Main df
 df = pd.read_csv('./data/df_joined.csv', index_col=False)
+df.fillna('', inplace=True)
 df.head()
 
 #Category tag and description
 with open('./data/dict_category.json', 'r') as f:
     cat = json.load(f)
-    print(cat)
 
 #Combine title, overview, and description columns
 ats = df['title'].str.cat(df['overview'], sep='. ').str.cat(df['description'], sep='. ')
@@ -27,22 +27,53 @@ ats = df['title'].str.cat(df['overview'], sep='. ').str.cat(df['description'], s
 ats_token = [nltk.word_tokenize(at) for at in ats]
 ats_tag = [nltk.pos_tag(at) for at in ats_token]
 
-#Preserve only nouns and [a-zA-Z0-9\-]
+#Combine NNP
+def combineNNP(at):
+    at.append(('$', 'END'))
+    at_combinedNNP = []
+    word_prev, tag_prev = '', ''
+    for (word, tag) in at:
+        if tag == 'NNP' and tag_prev == 'NNP':
+            word_prev = word_prev + '_' + word
+            continue
+
+        if tag != 'NNP':
+            if tag_prev == 'NNP': at_combinedNNP.append((word_prev, tag_prev))
+            at_combinedNNP.append((word, tag))
+
+        word_prev, tag_prev = word, tag
+        
+    return at_combinedNNP
+
+#Preserve only nouns
 def filterTag(targetTags, at):
     at_filteredTag = ((word, tag) for (word, tag) in at if tag in targetTags)
     return at_filteredTag
 
-def filterWord(pattern, at):
-    at_filteredWord = ((word, tag) for (word, tag) in at if re.search(pattern, word))
+#Preserve only [a-zA-Z0-9_\-]
+#Lower words as well
+def filter8LowerWord(pattern, at):
+    at_filteredWord = ((word.lower(), tag) for (word, tag) in at if re.search(pattern, word))
     return at_filteredWord
 
-ats_filtered = [list(filterWord('^[a-zA-Z0-9\-]+$', filterTag(['NN', 'NNP', 'NNS'], at))) for at in ats_tag]
+#Exclude specific words
+def filterSWords(sWords, at):
+    at_filteredSWords = ((word, tag) for (word, tag) in at if word not in sWords)
+    return at_filteredSWords
 
 #Combine POS and word
 def appendTag(at):
     at_tagAppended = (word + '_' + tag for (word, tag) in at)
     return at_tagAppended
 
+#Execution
+sWords = ['https']
+ats_filtered = [list(
+    filterSWords(sWords,
+    filter8LowerWord('^[a-zA-Z0-9_\-]+$',
+    filterTag(['NN', 'NNP', 'NNS'],
+    combineNNP(at
+))))) for at in ats_tag]
 ats_tagAppended = [list(appendTag(at)) for at in ats_filtered]
 
 
@@ -65,4 +96,24 @@ def filterTfIdf(at, threshold, dct):
     at_tfIdfFiltered = ((dct[idx], score) for (idx, score) in at if score >= threshold)
     return at_tfIdfFiltered
 
-ats_tfIdfFiltered = [list(filterTfIdf(at, 0.2, dct)) for at in ats_tfIdf]
+ats_tfIdfFiltered = [list(filterTfIdf(at, 0.15, dct)) for at in ats_tfIdf]
+
+
+#--Observation
+def observe(idx):
+    print(ats[idx])
+    print('-' * 60)
+    print(ats_tfIdfFiltered[idx])
+
+observe(170)
+
+
+#--Export
+df_joined.to_csv('./data/df_joined.csv', index=False)
+with open('./data/dict_category.json', 'w') as f:
+    json.dump(dict_category, f)
+
+#Test loading data back
+pd.read_csv('./data/df_joined.csv', index_col=False).head()
+with open('./data/dict_category.json', 'r') as f:
+    print(json.load(f))
